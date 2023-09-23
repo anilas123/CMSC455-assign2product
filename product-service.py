a1f383f6d6b41a6396f82bc891cf3c457adb32ad
anilas123 <anilas@vcu.edu>
2023-09-23 00:01:47 -0400

    New Method in Product

--- product-service.py
+++ product-service.py
@@ -26,7 +26,7 @@ def get_products():
 
 @app.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    product = products.get(product_id)
+    product = products.get(str(product_id))
     if product:
         return jsonify(product)
     else:
@@ -49,6 +49,22 @@ def add_product():
     products[new_product_id] = new_product
 
     return jsonify({"message": "Product added successfully", "product": new_product}), 201
+
+@app.route('/products/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    data = request.json
+
+    product = products.get(product_id)
+    if not product:
+        return jsonify({"error": "Product not found"}), 404
+
+    # Update the product properties with the new data
+    product["name"] = data.get('name', product["name"])
+    product["price"] = data.get('price', product["price"])
+    product["quantity"] = data.get('quantity', product["quantity"])
+
+    return jsonify({"message": "Product updated successfully", "product": product}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5049)
